0f51035ae3b624fdefcb340340677c933c50f533
varshaverse <varshaverse247@gmail.com>
2025-04-03 16:29:20 -0700

    changed name of variable

--- majority.py
+++ majority.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: int
         """
         element_counts = Counter(nums) #establish a counter
-        elem_dict = dict(element_counts) #check dictionary
-        for k,v in elem_dict.items(): #for loop
+        element_dict = dict(element_counts) #check dictionary
+        for k,v in element_dict.items(): #for loop
             if v >len(nums)/2: # check the condition again, why v ask urself, v tells us how many times it appears? 
                 return k
         
